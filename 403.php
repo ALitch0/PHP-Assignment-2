26a0c5ba719f930abdc6f363985ab50435961658
ALitch0 <123614243+ALitch0@users.noreply.github.com>
2023-04-09 12:39:12 -0400

    added error pages

---
+++ 403.php
@@ -0,0 +1,10 @@
+<?php
+$title = 'forbidden';
+require('shared/header.php');
+?>
+<main>
+<h1>Access Denied</h1>
+</main>
+<?php
+require ('shared/footer.php');
+?>
---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php
+require('shared/header.php');
+?>
+    <h1>PAGE NOT FOUND</h1>
+<?php
+require('shared/footer.php');
+?>
